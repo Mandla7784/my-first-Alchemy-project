0d54fdf5d1c62888fb10fb4be68bba576e2b0fa9
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-06-18 14:54:57 +0200

    working on Routes

--- app.py
+++ app.py
@@ -7,8 +7,26 @@ from flask_sqlalchemy import SQLAlchemy
 #My App
 
 app = Flask(__name__)
-# app.config("SQLALCHEMY_DATABASE_URI") = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db = SQLAlchemy(app)
 
+db.init_app(app)
+
+
+
+class TaskModel(db.Model):
+    
+    id = db.Column(db.Integer , primary_key = True)
+    task_Name = db.Column(db.String(50) , nullable=False)
+    
+    
+    def __repr__(self):
+        return f"Task({self.task_Name})"
+    
+
+with app.app_context():
+    db.create_all()
+    
 
 """_summary_
     here we using flask like a hub for our paged 
@@ -18,12 +36,26 @@ app = Flask(__name__)
     """
 
 #Routes
-@app.route("/")
+@app.route("/" , methods=['GET'])
 def index():
     
     return render_template("index.html")
 
 
+#create a new task
+
+@app.route("/tasks/create/" , methods=['GET','POST'])
+def create_task():
+    pass
+
+
+
+
+#Takss getting all tasks
+@app.route("/tasks/")
+def task_list():
+    tasks = db.session.execute(db.select(TaskModel).order_by(TaskModel.task_Name)).scalars()
+    return render_template("user/list.html", tasks = tasks)
 
 def main():
     app.run(debug=True);
