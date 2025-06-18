bb6b77a44e6883268377da18d5b12a02edd08d3e
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-06-18 13:22:09 +0200

    added index function

--- app.py
+++ app.py
@@ -8,4 +8,11 @@ from flask_sqlalchemy import SQLAlchemy
 #My App
 
 app = Flask(__name__)
-app.config("SQLALCHEMY_DATABASE_URI") = ""
+# app.config("SQLALCHEMY_DATABASE_URI") = ""
+
+def index():
+    
+    return "Testing 123"
+
+
+
