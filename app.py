a372dc67f3ed9e4e0f48d004ed998ad72e6cdfe8
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-06-18 13:27:22 +0200

    added templates folder for our html pages and static folder for our css and javascript

--- app.py
+++ app.py
@@ -3,15 +3,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 #My App
 
 app = Flask(__name__)
 # app.config("SQLALCHEMY_DATABASE_URI") = ""
 
 
-
+"""_summary_
+ here we using flask like a hub for our paged 
+ 
+    Returns:
+        _type_: _description_
+    """
 
 
 
